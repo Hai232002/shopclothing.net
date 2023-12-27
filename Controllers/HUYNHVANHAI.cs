e760fa32a2dc6eafe9fa664397d7f0ccfff958f5
Hai232002 <haihuynh.23032002@gmail.com>
2023-12-27 16:37:44 +0700

    upload project

---
+++ Controllers/HUYNHVANHAI.cs
@@ -0,0 +1,28 @@
+﻿using ClothesWebNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ClothesWebNET.Controllers
+{
+    
+    public class HUYNHVANHAI : Controller
+    {
+        private CLOTHESEntities db = new CLOTHESEntities();
+        //huynhvanhai
+        public ActionResult index()
+        {
+
+            return View();
+        }
+        public ActionResult hvh_2120110218()
+        {
+            return View(db.asp_2120110218.ToList());
+        }
+    }
+  
+}
